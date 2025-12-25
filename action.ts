a7cc11d60e6fac28d34994784c0845e9d6a6e499
Abdulla Agnigundala <110605472+Rahim5357@users.noreply.github.com>
2025-12-25 16:06:10 +0530

    Create action.ts

---
+++ action.ts
@@ -0,0 +1,14 @@
+// Dynamic Action Types Factory to generate action types for multiple APIs
+export const ActionTypesFactory = (actionName: string) => {
+  const actionPrefix = actionName ? actionName?.toUpperCase() : "";
+  return {
+    REQUEST: `${actionPrefix}_REQUEST`, // Action type for requesting data
+    SUCCESS: `${actionPrefix}_SUCCESS`, // Action type for successful API response
+    FAILED: `${actionPrefix}_FAILED`, // Action type for API failure
+    RESET: `${actionPrefix}_RESET`, // Action type for API reset
+    STORE: `${actionPrefix}_STORE`, // Action type for API reset
+  };
+};
+
+
+export const configsAction = ActionTypesFactory("configs");
